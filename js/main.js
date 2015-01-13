266715d499201820eb269ac6fb0363af4d35e63b
Mattias Olsson <mattias.olsson@hotmail.se>
2015-01-13 23:57:16 +0100

    updates

--- js/main.js
+++ js/main.js
@@ -21,24 +21,24 @@ $(document).ready(function(){
       };
     };
 
+    // Helper function to randomize hex numbers.
+    function randomizeColor() {
+      var str, 
+      colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
+      str = colors[(Math.floor(Math.random() * 15) + 1)];
+      str += colors[(Math.floor(Math.random() * 15) + 1)];
+      str += colors[(Math.floor(Math.random() * 15) + 1)];
+      str += colors[(Math.floor(Math.random() * 15) + 1)];
+      str += colors[(Math.floor(Math.random() * 15) + 1)];
+      str += colors[(Math.floor(Math.random() * 15) + 1)];
+      return '#' + str;
+    }
+
   }) (jQuery);
 
   $('.change-colors').click(function(){
     $(this).changeColor();
   });
-
-  // Helper function to randomize hex numbers.
-  function randomizeColor() {
-    var str, 
-    colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
-    str = colors[(Math.floor(Math.random() * 15) + 1)];
-    str += colors[(Math.floor(Math.random() * 15) + 1)];
-    str += colors[(Math.floor(Math.random() * 15) + 1)];
-    str += colors[(Math.floor(Math.random() * 15) + 1)];
-    str += colors[(Math.floor(Math.random() * 15) + 1)];
-    str += colors[(Math.floor(Math.random() * 15) + 1)];
-    return '#' + str;
-  }
 
 }); // document ready end. 
 
